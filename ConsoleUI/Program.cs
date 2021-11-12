63e0ca7d0e46d59967d7d0f884a001cb564f51b7
Michael Robards <mikerobards@icloud.com>
2021-11-12 08:47:04 -0500

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using DemoLibrary.Models;
+using System;
+
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel();
+
+            
+
+        }
+    }
+}
